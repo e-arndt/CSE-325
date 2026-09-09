f19ff2e971d79d040f09563521fe2a90222d0377
Eric Arndt <earndt1701@gmail.com>
2026-09-09 02:33:35 -0700

    Complete W02 MVC movie assignment

---
+++ W02/MVC-Movie/Models/MovieOptions.cs
@@ -0,0 +1,36 @@
+namespace MvcMovie.Models;
+
+public static class MovieOptions
+{
+    public static readonly string[] Genres =
+    [
+        "Action",
+        "Adventure",
+        "Animation",
+        "Comedy",
+        "Crime",
+        "Documentary",
+        "Drama",
+        "Family",
+        "Fantasy",
+        "Horror",
+        "Musical",
+        "Mystery",
+        "Romance",
+        "Romantic Comedy",
+        "Science Fiction",
+        "Thriller",
+        "War",
+        "Western"
+    ];
+
+    public static readonly string[] Ratings =
+    [
+        "G",
+        "PG",
+        "PG-13",
+        "R",
+        "NC-17",
+        "NR"
+    ];
+}
